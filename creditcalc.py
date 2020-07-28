4d5489245842ea0f0d9b62008a7410c40b517db9
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-07-28 15:09:06 +0600

    Stage 2/4
    Complete second stage

--- creditcalc.py
+++ creditcalc.py
@@ -1,12 +1,29 @@
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
+import math
 
-print(f"""{credit_principal}
-{first_month}
-{second_month}
-{third_month}
-{final_output}
+
+print("Enter the credit principal:")
+principal = int(input())
+print("""What do you want to calculate? 
+type "m" - for the count of months, 
+type "p" - for the monthly payment:
 """)
+type_calculate = input()
+if type_calculate.__eq__("p"):
+    print("Enter the count of months:")
+    count_of_months = int(input())
+    monthly_payment = principal / count_of_months
+    last_payment = principal - (count_of_months - 1) * math.ceil(monthly_payment)
+    if monthly_payment == last_payment:
+        print(f"Your monthly payment = {monthly_payment}")
+    else:
+        print(f"Your monthly payment = {math.ceil(monthly_payment)} with last month payment = {last_payment}")
+elif type_calculate.__eq__("m"):
+    print("Enter the monthly payment:")
+    monthly_payment = int(input())
+    months = math.ceil(principal / monthly_payment)
+    if months == 1:
+        print(f"""
+            It takes {months} month to repay the credit""")
+    else:
+        print(f"""
+            It takes {months} months to repay the credit""")
