231be175f365effe8613b75f734235def44c5b10
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-07-28 21:42:08 +0600

    Stage 4/4
    Complete project

--- creditcalc.py
+++ creditcalc.py
@@ -1,40 +1,50 @@
+import argparse
 import math
 
-
-print("""What do you want to calculate? 
-type "n" for the count of months, 
-type "a" for the annuity monthly payment,
-type "p" for credit principal:""")
-type_calculate = input()
-if type_calculate.__eq__("n"):
-    print("Enter the credit principal:")
-    credit_principal = int(input())
-    print("Enter monthly payment:")
-    annuity_payment = float(input())
-    print("Enter credit interest:")
-    credit_interest = float(input())
-    i = credit_interest / (12 * 100)
-    n = math.ceil(math.log((annuity_payment / (annuity_payment - i * credit_principal)), i+1))
+parser = argparse.ArgumentParser()
+parser.add_argument('--type', type=str)
+parser.add_argument('--principal', type=int)
+parser.add_argument('--periods', type=int)
+parser.add_argument('--payment', type=float)
+parser.add_argument('--interest', type=float)
+args = parser.parse_args()
+type_ = args.type
+principal = args.principal
+periods = args.periods
+payment = args.payment
+interest = args.interest
+if (type_ is not None) and (payment is not None) and (periods is not None) and (interest is not None):
+    i = interest / (12 * 100)
+    p = math.ceil(payment / (i * math.pow(1 + i, periods) / (math.pow(1 + i, periods) - 1)))
+    print(f"Your credit principal = {p}!")
+elif (type_ is not None) and (principal is not None) and (periods is not None) and (interest is not None):
+    i = interest / (12 * 100)
+    if type_.__eq__("annuity"):
+        a = math.ceil(principal * (i * math.pow(1 + i, periods) / (math.pow(1 + i, periods) - 1)))
+        print(f"Your annuity payment = {a}!")
+    else:
+        overpayment = 0
+        m = 1
+        for k in range(periods):
+            D = math.ceil(principal / periods + i * (principal - (principal * (m - 1) / periods)))
+            overpayment += D
+            print(f"Month {m}: paid out {D}")
+            m += 1
+        print(f"Overpayment = {overpayment - principal}")
+elif (type_ is not None) and (principal is not None) and (payment is not None) and (interest is not None):
+    i = interest / (12 * 100)
+    n = math.ceil(math.log((payment / (payment - i * principal)), i + 1))
+    overpayment = int(payment * n - principal)
     years = n // 12
     months = n - years * 12
-    print(f"You need {years} years and {months} months to repay this credit!")
-elif type_calculate.__eq__("a"):
-    print("Enter the credit principal:")
-    credit_principal = int(input())
-    print("Enter count of periods:")
-    count_of_periods = float(input())
-    print("Enter credit interest:")
-    credit_interest = float(input())
-    i = credit_interest / (12 * 100)
-    a = math.ceil(credit_principal * (i * math.pow(1+i, count_of_periods) / (math.pow(1+i, count_of_periods) - 1)))
-    print(f"Your annuity payment = {a}!")
-elif type_calculate.__eq__("p"):
-    print("Enter monthly payment:")
-    annuity_payment = float(input())
-    print("Enter count of periods:")
-    count_of_periods = float(input())
-    print("Enter credit interest:")
-    credit_interest = float(input())
-    i = credit_interest / (12 * 100)
-    p = math.ceil(annuity_payment / (i * math.pow(1 + i, count_of_periods) / (math.pow(1 + i, count_of_periods) - 1)))
-    print(f"Your credit principal = {p}!")
+    if months == 0:
+        print(f"You need {years} years to repay this credit!")
+        print(f"Overpayment = {overpayment}")
+    elif years == 0:
+        print(f"You need {months} months to repay this credit!")
+        print(f"Overpayment = {overpayment}")
+    else:
+        print(f"You need {years} years and {months} months to repay this credit!")
+        print(f"Overpayment = {overpayment}")
+else:
+    print("Incorrect parameters.")
